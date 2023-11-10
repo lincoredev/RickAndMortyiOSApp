79304fb343bb275a49587c6bb46e6b3b14800eda
Vova Robot <lincore14@mail.ru>
2023-11-10 22:14:32 +0300

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Vova Lincore on 10.11.2023.
+//
+
+import Foundation
+
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
--- RickAndMorty/Controllers/Core/RMTabViewController.swift
+++ RickAndMorty/Controllers/Core/RMTabViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+/// Controller to house tabs and root tab controllers 
 final class RMTabViewController: UITabBarController {
 
     override func viewDidLoad() {
